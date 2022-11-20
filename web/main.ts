72f622a94c0d116645e9b0cc61b5e02b7d732f4b
Evan Martin <evan.martin@gmail.com>
2022-11-19 16:02:43 -0800

    schema

--- web/main.ts
+++ web/main.ts
@@ -1,101 +1,11 @@
+import { Tables } from "./table";
 
-interface TableMeta {
-  type: string;
-}
-interface Meta {
-  rows: number;
-  tables: { [name: string]: TableMeta };
-}
-
-class Tables {
-  private constructor(readonly rows: number, readonly tables: { [name: string]: unknown }) { }
-
-  static async load(root: string): Promise<Tables> {
-    const path = `${root}/meta`;
-    const req = await fetch(path);
-    if (!req.ok) {
-      throw new Error(`load ${path} failed`);
-    }
-    const meta: Meta = await req.json();
-
-    const tables: { [name: string]: unknown } = {};
-    const loads: Array<Promise<void>> = [];
-    for (const [name, tmeta] of Object.entries(meta.tables)) {
-      const path = `${root}/${name}`;
-      switch (tmeta.type) {
-        case 'num':
-          loads.push((async () => { tables[name] = await NumTable.load(path, meta.rows); })());
-          break;
-        case 'str':
-          loads.push((async () => { tables[name] = await StrTable.load(path, meta.rows); })());
-          break;
-        default:
-          throw new Error(`unknown table type ${JSON.stringify(tmeta.type)}`);
-      }
-    }
-    await Promise.all(loads);
-    return new Tables(meta.rows, tables);
-  }
-}
-
-class NumTable {
-  constructor(readonly tab: Uint32Array, readonly rows: number) { }
-  static async load(path: string, rows: number): Promise<NumTable> {
-    const req = await fetch(path);
-    if (!req.ok) {
-      throw new Error(`load ${path} failed`);
-    }
-    const raw = await req.arrayBuffer();
-    return new NumTable(new Uint32Array(raw), rows);
-  }
-
-  count(): Map<number, number> {
-    const counts = new Map<number, number>();
-    for (const value of this.tab) {
-      counts.set(value, (counts.get(value) ?? 0) + 1);
-    }
-    return counts;
-  }
-
-  top(n: number): Array<{ value: number, count: number }> {
-    const top = [];
-    for (const [value, count] of this.count().entries()) {
-      top.push({ value, count });
-    }
-    top.sort(({ count: a }, { count: b }) => b - a);
-    return top.slice(0, n);
-  }
-}
-
-class StrTable {
-  constructor(readonly numTab: NumTable, readonly strs: string[]) { }
-
-  static async load(path: string, rows: number): Promise<StrTable> {
-    const req = await fetch(path);
-    if (!req.ok) {
-      throw new Error(`load ${path} failed`);
-    }
-    const raw = await req.arrayBuffer();
-    const numTab = new NumTable(new Uint32Array(raw.slice(0, 4 * rows)), rows);
-    const json = new TextDecoder().decode(new DataView(raw, numTab.tab.byteLength));
-    const strings = JSON.parse(json);
-    return new StrTable(numTab, strings);
-  }
-
-  top(n: number): Array<{ value: string, count: number }> {
-    const top = [];
-    for (const { value, count } of this.numTab.top(n)) {
-      top.push({ value: this.strs[value], count });
-    }
-    return top;
-  }
-}
+const SCHEMA = { 'date': 'num', 'path': 'str' } as const;
 
 async function main() {
-  const tables = await Tables.load("tab");
+  const tables = await Tables.load(SCHEMA, "tab");
   console.log(tables);
-  const paths = tables.tables['path'] as StrTable;
-  console.log(paths.top(20));
+  console.log(tables.tables.path.top(20));
 }
 
 main().catch(err => console.log(err));
---
+++ web/table.ts
@@ -0,0 +1,111 @@
+
+interface TableMeta {
+  type: string;
+}
+interface Meta {
+  rows: number;
+  tables: { [name: string]: TableMeta };
+}
+
+type SchemaTable<T> = T extends 'num' ? NumTable : T extends 'str' ? StrTable : never;
+type SchemaTables<S> = {
+  [table in keyof S]: SchemaTable<S[table]>;
+}
+
+async function loadCheckMeta<S extends { [table: string]: string }>(schema: S, root: string): Promise<Meta> {
+  const path = `${root}/meta`;
+  const req = await fetch(path);
+  if (!req.ok) {
+    throw new Error(`load ${path} failed`);
+  }
+  const meta: Meta = await req.json();
+
+  const tables: { [name: string]: unknown } = {};
+  const loads: Array<Promise<void>> = [];
+  for (const [name, type] of Object.entries(schema)) {
+    const tmeta = meta.tables[name];
+    if (!tmeta) {
+      throw new Error(`missing table ${name} in meta`);
+    }
+    if (tmeta.type !== type) {
+      throw new Error(`table ${name}: expected ${type}, got ${tmeta.type}`);
+    }
+  }
+  return meta;
+}
+
+export class Tables<S> {
+  private constructor(readonly rows: number, readonly tables: SchemaTables<S>) { }
+
+  static async load<S extends { [table: string]: string }>(schema: S, root: string): Promise<Tables<S>> {
+    const meta = await loadCheckMeta(schema, root);
+    const tables: { [name: string]: unknown } = {};
+    const loads: Array<Promise<void>> = [];
+    for (const [name, type] of Object.entries(schema)) {
+      const path = `${root}/${name}`;
+      switch (type) {
+        case 'num':
+          loads.push((async () => { tables[name] = await NumTable.load(path, meta.rows); })());
+          break;
+        case 'str':
+          loads.push((async () => { tables[name] = await StrTable.load(path, meta.rows); })());
+          break;
+      }
+    }
+    await Promise.all(loads);
+    return new Tables(meta.rows, tables as SchemaTables<S>);
+  }
+}
+
+class NumTable {
+  constructor(readonly tab: Uint32Array, readonly rows: number) { }
+  static async load(path: string, rows: number): Promise<NumTable> {
+    const req = await fetch(path);
+    if (!req.ok) {
+      throw new Error(`load ${path} failed`);
+    }
+    const raw = await req.arrayBuffer();
+    return new NumTable(new Uint32Array(raw), rows);
+  }
+
+  count(): Map<number, number> {
+    const counts = new Map<number, number>();
+    for (const value of this.tab) {
+      counts.set(value, (counts.get(value) ?? 0) + 1);
+    }
+    return counts;
+  }
+
+  top(n: number): Array<{ value: number, count: number }> {
+    const top = [];
+    for (const [value, count] of this.count().entries()) {
+      top.push({ value, count });
+    }
+    top.sort(({ count: a }, { count: b }) => b - a);
+    return top.slice(0, n);
+  }
+}
+
+class StrTable {
+  constructor(readonly numTab: NumTable, readonly strs: string[]) { }
+
+  static async load(path: string, rows: number): Promise<StrTable> {
+    const req = await fetch(path);
+    if (!req.ok) {
+      throw new Error(`load ${path} failed`);
+    }
+    const raw = await req.arrayBuffer();
+    const numTab = new NumTable(new Uint32Array(raw.slice(0, 4 * rows)), rows);
+    const json = new TextDecoder().decode(new DataView(raw, numTab.tab.byteLength));
+    const strings = JSON.parse(json);
+    return new StrTable(numTab, strings);
+  }
+
+  top(n: number): Array<{ value: string, count: number }> {
+    const top = [];
+    for (const { value, count } of this.numTab.top(n)) {
+      top.push({ value: this.strs[value], count });
+    }
+    return top;
+  }
+}
