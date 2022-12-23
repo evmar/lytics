29753c698803c2726f2dd6544a37dd2aa2ea1527
Evan Martin <evan.martin@gmail.com>
2022-12-22 18:22:57 -0800

    more detailed stats

--- ingest/stats.sh
+++ ingest/stats.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-for f in ../tab/*; do
-    size=$(gzip -c $f | wc -c)
-    echo "$f $size"
+for f in ../tab/*.js; do
+    size=$(wc -c $f | awk '{print $1}')
+    gzsize=$(gzip -c $f | wc -c | awk '{print $1}')
+    pct=$((gzsize * 100 / size))
+    echo "$f $size => $gzsize $pct%"
 done
